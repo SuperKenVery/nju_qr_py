28af29474ce72a39a56ce312c1802c27c45788f5
ken <1610057945@qq.com>
2025-07-13 23:23:45 +0800

    Make it work on 2025/7/13

--- njuQrCode.py
+++ njuQrCode.py
@@ -1,57 +1,41 @@
 import requests,json
 
-def getQrContent(login_cookie):
+def getQrContent(login_cookie: str) -> str:
     '''
     login_cookie: The cookie "CASTGC" when logging in at authserver.nju.edu.cn
     example: login_cookie="CASTGC=ekdgjwvwlr;"
+
+    Returns the base64 of qrcode (qrcode is an image 😅)
     '''
-    if login_cookie[:7]!='CASTGC=':
-        login_cookie='CASTGC='+login_cookie
-    ua="Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 (5511528960)cpdaily/9.0.14  wisedu/9.0.14"
-    session_response_1=requests.get(
-        url="https://authserver.nju.edu.cn/authserver/login?service=https%3A%2F%2Fqrcode.nju.edu.cn%3A443%2Fapi%2Fh5",
-        headers={
-            'cookie':login_cookie,
-            'user-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 (4504969728)cpdaily/9.0.14  wisedu/9.0.14'
-        },
-        allow_redirects=False
+
+    if login_cookie.startswith("CASTGC="):
+        login_cookie = login_cookie[7:]
+
+    session = requests.Session()
+    # session.verify = False
+    session.cookies.update({"CASTGC": login_cookie})
+    session.headers.update({"user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 (4504969728)cpdaily/9.0.14  wisedu/9.0.14"})
+
+    # Get some cookies
+    # qrcode -> authserver -> qrcode
+    session.get("https://qrcode.nju.edu.cn")
+
+    ticket = session.post("https://qrcode.nju.edu.cn/api/check").text
+    session.headers.update({"Ticket": ticket})
+
+    qr_exchange_resp = session.post("https://qrcode.nju.edu.cn/code/v1/qrexchange")
+    qr_exchange = qr_exchange_resp.json()
+    session.headers.update({"Authorization": qr_exchange['jwt']})
+
+    qr_data_resp = session.post(
+        "https://qrcode.nju.edu.cn/code/v1/qrcode?t=1752405961085",
+        json={"stuempno": None}
     )
-    session_redirect_1=session_response_1.headers['Location']
+    qr_code_data = qr_data_resp.json()['qrcode']
 
-    session_response_2=requests.get(url=session_redirect_1,headers={'user-agent':ua},allow_redirects=False)
-    session_redirect_2=session_response_2.headers['Location']
+    return qr_code_data
 
-    session_response_3=requests.get(url=session_redirect_2,headers={'user-agent':ua},cookies=session_response_2.cookies,allow_redirects=False)
-    session_cookie=session_response_3.cookies
 
-    ticket_response=requests.post(
-        url="https://qrcode.nju.edu.cn/api/check",
-        cookies=session_cookie,
-        data=""
-    )
-    ticket=ticket_response.text
-
-    bearer_token_response=requests.post(
-        url="https://qrcode.nju.edu.cn/code/v1/qrexchange",
-        headers={
-            'ticket':ticket
-        },
-        data=""
-    )
-    bearer_token_json=bearer_token_response.text
-    bearer_token=json.loads(bearer_token_json)['jwt']
-
-    qr_content_response=requests.post(
-        url="https://qrcode.nju.edu.cn/code/v1/qrcode",
-        headers={
-            'authorization':bearer_token
-        },
-        data=""
-    )
-    qr_content_json=qr_content_response.text
-    qr_content=json.loads(qr_content_json)['qrcode']
-
-    return qr_content
 
 if __name__=='__main__':
     # print(getQrContent("CASTGC=TGT-196633-FhxLaCBjUsdfsdscvxwwlv4eeggreregUqbpnbhpYkBELrralM91660144576435-wMv1-cas;"))
